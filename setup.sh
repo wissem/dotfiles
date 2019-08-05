c7f69fbb61f5128f6684a37195a0066b5a03fa4f
Wissem Riahi <wissemr@gmail.com>
2019-08-05 14:38:12 +0200

    updates

--- setup.sh
+++ setup.sh
@@ -7,3 +7,16 @@ PWD=`pwd`
 ln -s $PWD/.bash_profile ~/
 ln -s $PWD/.bash_alias ~/
 ln -s $PWD/.zshrc ~/
+
+
+# install homebrew
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install zsh
+brew install zsh
+
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# todo: install iTerm2, setup git, PHPStorm settings, install docker for mac, download and install sublime text
